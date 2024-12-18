9171be759589b793e4d28a7510fe5db8452f405c
notrssa <carissa.lidra@smkn1kotabekasi.sch.id>
2024-12-18 10:21:07 +0700

    update

--- main.js
+++ main.js
@@ -18,17 +18,34 @@ $(document).ready(function () {
 
 AOS.init();
 
-document.addEventListener("DOMContentLoaded", function () {
-  const audioPlayer = document.getElementById("audio-player");
-  const muteButton = document.getElementById("mute-button");
-  const muteIcon = muteButton.querySelector("i");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const audioPlayer = document.getElementById("audio-player");
+//   const muteButton = document.getElementById("mute-button");
+//   const muteIcon = muteButton.querySelector("i");
 
-  muteButton.addEventListener("click", function () {
+//   muteButton.addEventListener("click", function () {
+//     if (audioPlayer.muted) {
+//       audioPlayer.muted = false;
+//       muteIcon.classList.remove("fa-volume-mute");
+//       muteIcon.classList.add("fa-volume-up");
+//       muteButton.setAttribute("aria-label", "Unmute");
+//     } else {
+//       audioPlayer.muted = true;
+//       muteIcon.classList.remove("fa-volume-up");
+//       muteIcon.classList.add("fa-volume-mute");
+//       muteButton.setAttribute("aria-label", "Mute");
+//     }
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const audioPlayer = document.getElementById('audio-player');
+  const muteButton = document.getElementById('mute-button');
+  const muteIcon = muteButton.querySelector('i');
+
+  muteButton.addEventListener('click', function () {
     if (audioPlayer.muted) {
-      audioPlayer.muted = false;
-      muteIcon.classList.remove("fa-volume-mute");
-      muteIcon.classList.add("fa-volume-up");
-      muteButton.setAttribute("aria-label", "Unmute");
+      muteButton.setAttribute('aria-label', "Unmute");
     } else {
       audioPlayer.muted = true;
       muteIcon.classList.remove("fa-volume-up");
@@ -37,3 +54,25 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+let audio = document.getElementById('audio');
+let play = document.getElementById('play');
+let mute = document.getElementById('mute-button');
+
+play.addEventListener('click', function() {
+    audio.play();
+});
+
+mute.addEventListener('click', function() {
+    audio.muted = !audio.muted;
+})
+
+function PlayMusic() {
+
+  var play=document.getElementById("audio");
+  play.play();
+}
+
+$(document).ready(function(){
+  setTimeout(PlayMusic,3000);
+})
